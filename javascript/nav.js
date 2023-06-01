35de8fa40ab527d8ce5aef71cb79c2d80b2303c8
2023geo <georgesailesmbewe@gmail.com>
2023-06-01 15:51:07 +0200

    updated

---
+++ javascript/nav.js
@@ -0,0 +1,18 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+//menu accordion 
+var acc = document.getElementsByClassName("accordiona");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+  }
+}
